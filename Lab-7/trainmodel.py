e8e406c4c0d16780d10adf6d4defbe9958b43a3f
Calandrinon <georgesut@yahoo.com>
2021-05-03 17:10:32 +0300

    Add graph displaying the loss values at every 100 epochs

--- Lab-7/trainmodel.py
+++ Lab-7/trainmodel.py
@@ -1,22 +1,24 @@
 import math, torch
 from myModel import ApproximationNeuralNetwork
+import matplotlib.pyplot as plt
 
 trainingDataTensor = torch.load("mydataset.dat")
 inputValues = trainingDataTensor[:,:2]
 givenFunctionValues = trainingDataTensor[:,2:]
 
-lossFunction  = torch.nn.MSELoss()
+lossFunction = torch.nn.MSELoss()
 inputLayerSize = 2
 hiddenLayerSize = 10
 outputLayerSize = 1
 approximationNeuralNetwork = ApproximationNeuralNetwork(inputLayerSize, hiddenLayerSize, outputLayerSize)
 
-optimizer = torch.optim.SGD(approximationNeuralNetwork.parameters(), lr=0.05)
+optimizer = torch.optim.SGD(approximationNeuralNetwork.parameters(), lr=0.01)
 listOfErrors = []
 
 batchSize = 16
 numberOfBatches = int(len(trainingDataTensor) / batchSize)
 numberOfEpochs = 2000
+epochList = []
 
 for epochIndex in range(numberOfEpochs):
     for batchIndex in range(numberOfBatches):
@@ -25,16 +27,24 @@ for epochIndex in range(numberOfEpochs):
         predictedFunctionValue = approximationNeuralNetwork(inputBatch)
         lossFunctionValue = lossFunction(predictedFunctionValue, outputBatch)
 
-        listOfErrors.append(lossFunctionValue)
         optimizer.zero_grad()
         lossFunctionValue.backward()
         optimizer.step()
 
     if epochIndex % 100 == 99:
         predictedFunctionValues = approximationNeuralNetwork(inputValues)
-        lossFunctionValue = lossFunction(predictedFunctionValues, givenFunctionValues)
+        lossFunctionValue = lossFunction(predictedFunctionValues, givenFunctionValues).tolist()
+        listOfErrors.append(lossFunctionValue)
+        epochList.append(epochIndex)
+
+        plt.xlabel("Epoch")
+        plt.ylabel("Value of the loss function")
+        plt.plot(epochList, listOfErrors, color="green")
+        plt.legend(["Loss function values for each epoch"], labelcolor=["green"])
+        plt.pause(0.05)
         print("Epoch: {}; Loss: {}".format(epochIndex, lossFunctionValue))
-    
+
+plt.show()
 
 filename = "ApproximationNeuralNetwork.pt"
 torch.save(approximationNeuralNetwork.state_dict(), filename)

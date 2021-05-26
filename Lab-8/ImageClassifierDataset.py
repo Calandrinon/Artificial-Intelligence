9a4c8d598c514e8a282805524957521a9dd6df81
Calandrinon <georgesut@yahoo.com>
2021-05-26 09:04:06 +0300

    Add the ImageReader class which will read the dataset

--- Lab-8/ImageClassifierDataset.py
+++ Lab-8/ImageClassifierDataset.py
@@ -4,6 +4,9 @@ import torch.optim as optim
 import time
 from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, models, transforms
+from PIL import Image
+import os
+
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class ImageClassifierDataset(Dataset):
@@ -33,3 +36,56 @@ class ImageClassifierDataset(Dataset):
 
     def __len__(self):
         return len(self.images)
+
+
+class ImageReader:
+
+    def __init__(self, pathToDirectory):
+        self.__pathToDirectory = pathToDirectory 
+        self.__maleImages = []
+        self.__femaleImages = []
+        self.__thingsImages = []
+        self.__readImages()
+
+
+    def __readImages(self):
+        directories = os.listdir(self.__pathToDirectory)
+        for directory in directories:
+            listOfImages = os.listdir(self.__pathToDirectory + "/" + directory) 
+            for image in listOfImages:
+                if directory == "Men":
+                    self.__maleImages.append(Image.open(self.__pathToDirectory + "/" + directory + "/" + image))
+                elif directory == "Women":
+                    self.__femaleImages.append(Image.open(self.__pathToDirectory + "/" + directory + "/" + image))
+                else:
+                    self.__thingsImages.append(Image.open(self.__pathToDirectory + "/" + directory + "/" + image))
+        
+        print(" ============================================= Men ============================================= ")
+        print(self.__maleImages)
+
+        print(" ============================================= Women ============================================= ")
+        print(self.__femaleImages)
+        
+        print(" ============================================= Things ============================================= ")
+        print(self.__thingsImages)
+
+
+    def getImagesOfMen(self):
+        return self.__maleImages
+
+    def getImagesOfWomen(self):
+        return self.__femaleImages
+
+    def getImagesOfThings(self):
+        return self.__thingsImages
+
+
+imageReader = ImageReader("FacesDataset")
+someMen = imageReader.getImagesOfMen()
+someMen[0].show()
+
+someWomen = imageReader.getImagesOfWomen()
+someWomen[0].show()
+
+someThings = imageReader.getImagesOfThings()
+someThings[0].show()
